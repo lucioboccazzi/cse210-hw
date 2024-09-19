d6bc06bd5e63dd1688b723d06c0461882ca5e6b9
lucioboccazzi <lucioboccazzi@gmail.com>
2024-09-19 07:52:07 -0300

    s

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,70 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What was your score on the test? ");
+        string scoreInput = Console.ReadLine();
+
+        int score = int.Parse(scoreInput);
+
+        // if (score >= 90)
+        // {
+        //     Console.WriteLine("Your grade is A.");
+        // }
+        // else if (score >= 80 && score < 90)
+        // {
+        //     Console.WriteLine("Your grade is B.");
+        // }
+        // else if (score >= 70 && score < 80)
+        // {
+        //     Console.WriteLine("Your grade is C.");
+        // }
+        // else if (score >= 60 && score < 70)
+        // {
+        //     Console.WriteLine("Your grade is D.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Your grade is F.");
+        // }
+
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (score >= 80 && score < 90)
+        {
+            letter = "B";
+        }
+
+        else if (score >= 70 && score < 80)
+        {
+            letter = "C";
+        }
+
+        else if (score >= 60 && score < 70)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you have not passed. Go back and review your notes again, you can do it :)");
+        }
     }
+    
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is your magic number? ");
+        string magicNumberText = Console.ReadLine();
+
+        int magicNumber = int.Parse(magicNumberText);
+
+        Console.Write("What is your guess? ");
+        string guessNumberText = Console.ReadLine();
+
+        int guessNumber = int.Parse(guessNumberText);
+
+        if (magicNumber > guessNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("Youg guessed it!");
+        }
     }
 }
