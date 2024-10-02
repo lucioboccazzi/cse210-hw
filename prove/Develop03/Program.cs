4645f488eb738756e18c5ca23d441a2adf5989c0
lucioboccazzi <lucioboccazzi@gmail.com>
2024-10-02 07:22:48 -0300

    Prove 3 part 1

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+
+
+
+
+        
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class World
+{
+    private string _text;
+
+
+}
+    
